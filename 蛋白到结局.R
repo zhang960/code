3c09256d3a52d1f9126e923f58bcd51efa91c2fd
zhang960 <127748215+zhang960@users.noreply.github.com>
2024-08-24 11:40:38 +0800

    Update 蛋白到结局.R

--- 蛋白到结局.R
+++ 蛋白到结局.R
@@ -97,10 +97,5 @@ mr_leaveoneout_plot(leaveoneout_results = mr_leaveoneout(dat))
 dev.off()
 
 
-######生信自学网: https://www.biowolf.cn/
-######课程链接1: https://shop119322454.taobao.com
-######课程链接2: https://ke.biowolf.cn
-######课程链接3: https://ke.biowolf.cn/mobile
-######光俊老师邮箱: seqbio@foxmail.com
-######光俊老师微信: eduBio
+
 
